6eca544df23f6006ccca46973b9bfa56dc7b1b6a
Slava <slavapas13@gmail.com>
2015-04-08 16:51:36 +0300

    Init AngularJS Cookbook

---
+++ Chapter1/simple-element-directive-2.js
@@ -0,0 +1,14 @@
+﻿angular.module('myApp', [])
+.directive('elementDirective', function ($log) {
+    return {
+        restrict: 'E',
+        //replace: true,
+        template: '<p>Ze template!</p>',
+        link: function (scope, el, attrs) {
+            $log.log(el.html());
+            // Ze template!
+            $log.log(attrs.someAttr);
+            // myvalue
+        }
+    };
+});
---
+++ Chapter1/simple-element-directive.js
@@ -0,0 +1,10 @@
+﻿angular.module('myApp', [])
+.directive('myDirective', function () {
+    // return the directive definition object
+    return {
+        // only match this directive to element tags
+        restrict: 'E',
+        // insert the template matching 'my-template.html'
+        templateUrl: 'my-template.html'
+    };
+});
